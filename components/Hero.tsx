74710f1bc2222acb241ebb5266e506080c1c540c
fateme adiban <f.adiban.rad@gmail.com>
2026-02-16 22:42:12 +0330

    Fix hero section margin top

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import { HeroHeading, HeroSubHeading, HeroFirstButton, HeroSecondButton, HeroIma
 const Hero = () => {
   return (
     <section id="home" className="min-h-screen relative overflow-x-hidden flex flex-col lg:flex-row px-5 lg:px-10 pt-40 lg:pt-0 justify-between items-center lg:items-center">
-      <div className="flex flex-col gap-10 md:gap-9 lg:mt-20">
+      <div className="flex flex-col gap-10 md:gap-9 -mt-10 lg:mt-20">
         <h1 className="text-[#222222] albert_sans text-[40px] lg:text-[60px] xl:text-[70px] tracking-[-2%] leading-[110%] max-w-full sm:max-w-md lg:max-w-2xl xl:max-w-3xl">{HeroHeading}</h1>
 
         <h2 className="albert_sans text-[20px] lg:text-[24px] text-[#222222] opacity-70 tracking-[-2%] leading-[150%] max-w-[90%] md:max-w-md lg:max-w-xl 2xl:max-w-130">{HeroSubHeading}</h2>
